6e5df89d9bbdb31f92257c0f2a5a192fb0f1815f
NevesSantos <dexdex2000@hotmail.com>
2023-08-03 15:29:06 -0300

    update

--- script.js
+++ script.js
@@ -1,58 +1,44 @@
 let listaProdutos = [
     produto = {
-        imgsProduto: 'imgs/pao-frances.jpg',
         nomeProduto: 'Pão Francês',
-        descProduto: 'Veio da frança',
-        precoProduto: 'R$ 7,99'
+        imgProduto: 'img/pão-frances.jpg',
+        descProduto: 'Veio da françã',
+        precoProduto: 'R$ 8,99 Kg'
     },
+
     produto = {
-        imgsProduto: 'imgs/pao-queijo.jpg',
-        nomeProduto: 'Pâo de queijo',
-        descProduto: 'De queijo mesmo em ksksks',
-        precoProduto: 'R$ 10,00'
+        nomeProduto: 'Pão de Queijo',
+        imgProduto: 'img/pao-queijo.jpg',
+        descProduto: 'Veio do queijo',
+        precoProduto: 'R$ 10,99 Kg'
+    },
+
+    produto = {
+        nomeProduto: 'Pão Doce',
+        imgProduto: 'img/pao-doce.jpg',
+        descProduto: 'Veio da terra doce',
+        precoProduto: 'R$ 25,50 Kg'
+    },
+
+    produto = {
+        nomeProduto: 'Pão integral',
+        imgProduto: 'img/pao-integral.jpg',
+        descProduto: 'Veio do integrado',
+        precoProduto: 'R$ 20.99 Kg'
     },
 ];
-let divRow = document.createElement('div');
-if (listaProdutos.length > 0) {
-    for (let i = 0; i < listaProdutos.length; i++) {
 
-        let imgs = document.createElement('img');
-        imgs.classList.add('img-produto');
-        imgs.setAttribute('src', listaProdutos[i].imgsProduto);
-        imgs.setAttribute('alt', listaProdutos[i].nomeProduto);
+let tagImg = document.createElement('img');
+tagImg.classList.add('img-produto');
+tagImg.setAttribute('src', listaProdutos[0].imgProduto);
+tagImg.setAttribute('alt', listaProdutos[0].nomeProduto);
 
-        let divNomeProduto = document.createElement('div');
-        divNomeProduto.classList.add('nome-produto');
-        divNomeProduto.innerText = listaProdutos[i].nomeProduto;
+let divcol = document.createElement('div');
+divcol.classList.add('col-3');
 
-        let divDesc = document.createElement('div');
-        divDesc.classList.add('descricao');
-        divDesc.innerText = listaProdutos[i].descProduto;
+divcol.appendChild(tagImg);
 
-        let divPreco = document.createElement('div');
-        divPreco.classList.add('preco');
-        divPreco.innerText = listaProdutos[i].precoProduto;
-
-        let button = document.createElement('button');
-        button.classList.add('btn', 'btn-success');
-        button.innerText = "Comprar >>>";
-
-        let divCol = document.createElement('div');
-        divCol.classList.add('col-3', 'produto');
-        divCol.appendChild(imgs);
-        divCol.appendChild(divNomeProduto);
-        divCol.appendChild(divDesc);
-        divCol.appendChild(divPreco);
-        divCol.appendChild(button);
-
-
-        divRow.classList.add('row');
-        divRow.appendChild(divCol);
-
-    }
-} else {
-    divRow.innerText = "Nenhum elemento encontrado";
-}
 
 let divProdutos = document.getElementById('produtos');
-divProdutos.appendChild(divRow);
+
+console.log(divProdutos);
