4461ab3d56127455fe86d3a32c950badfd581aa9
NevesSantos <dexdex2000@hotmail.com>
2023-08-03 16:56:57 -0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let listaProdutos = [
     produto = {
         nomeProduto: 'Pão Francês',
-        imgProduto: 'img/pão-frances.jpg',
-        descProduto: 'Veio da françã',
+        imgProduto: 'img/pao-frances.jpg',
+        descProduto: 'Veio da frança',
         precoProduto: 'R$ 8,99 Kg'
     },
 
@@ -27,18 +27,37 @@ let listaProdutos = [
         precoProduto: 'R$ 20.99 Kg'
     },
 ];
+let divRow = document.createElement('div');
 
-let tagImg = document.createElement('img');
-tagImg.classList.add('img-produto');
-tagImg.setAttribute('src', listaProdutos[0].imgProduto);
-tagImg.setAttribute('alt', listaProdutos[0].nomeProduto);
+let img = document.createElement('img');
+img.classList.add('img-produto');
+img.setAttribute('src', listaProdutos[0].imgProduto);
+img.setAttribute('alt', listaProdutos[0].nomeProduto);
+
+let divNome = document.createElement('div');
+divNome.classList.add('nome-produto');
+divNome.innerText= (listaProdutos[0].nomeProduto);
+let divDesc = document.createElement('div');
+divDesc.classList.add('desc-produto');
+divDesc.innerText= (listaProdutos[0].descProduto);
+let divpreco = document.createElement('div');
+divpreco.classList.add('preco-produto');
+divpreco.innerText= (listaProdutos[0].precoProduto);
+let button = document.createElement('div');
+button.classList.add('btn')
 
 let divcol = document.createElement('div');
 divcol.classList.add('col-3');
+divcol.appendChild(img);
+divcol.appendChild(divNome);
+divcol.appendChild(divDesc);
+divcol.appendChild(divpreco);
 
-divcol.appendChild(tagImg);
 
+divRow.classList.add('row');
+divRow.appendChild(divcol);
 
 let divProdutos = document.getElementById('produtos');
+divProdutos.appendChild(divRow);
 
 console.log(divProdutos);
